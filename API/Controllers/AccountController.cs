8a9c64a2852c90c938ed8e54bc12bd60fb444b9c
Samin Ahmed <s.ahmedd99@gmail.com>
2025-08-26 15:00:00 +0100

    implement login functionality in AccountController

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -18,7 +18,7 @@ public class AccountController(AppDbContext context) : BaseApiController
         {
             return BadRequest("Email is taken");
         }
-        
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
@@ -31,6 +31,25 @@ public class AccountController(AppDbContext context) : BaseApiController
 
         context.Users.Add(user);
         await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    [HttpPost("login")] //api/account/login
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users
+            .SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+
+        if (user == null) return Unauthorized("Invalid email");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (var i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
 
         return user;
     }
