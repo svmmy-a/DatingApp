a52ccb8b0f649e757eefc672ce02c3548e49a76c
Samin Ahmed <s.ahmedd99@gmail.com>
2025-08-26 16:33:17 +0100

    refactor AccountController to return UserDto instead of AppUser and include token generation

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,14 +5,15 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using API.DTOs;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context) : BaseApiController
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] //api/account/register
 
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await EmailExists(registerDto.Email))
         {
@@ -23,8 +24,8 @@ public class AccountController(AppDbContext context) : BaseApiController
 
         var user = new AppUser
         {
-            Email = registerDto.Email,
             DisplayName = registerDto.DisplayName,
+            Email = registerDto.Email,
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
@@ -32,11 +33,17 @@ public class AccountController(AppDbContext context) : BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            Id = user.Id,
+            DisplayName = user.DisplayName,
+            Email = user.Email,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")] //api/account/login
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users
             .SingleOrDefaultAsync(x => x.Email == loginDto.Email);
@@ -51,7 +58,13 @@ public class AccountController(AppDbContext context) : BaseApiController
             if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
         }
 
-        return user;
+        return new UserDto
+        {
+            Id = user.Id,
+            DisplayName = user.DisplayName,
+            Email = user.Email,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> EmailExists(string email)
