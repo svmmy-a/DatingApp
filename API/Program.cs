0db93ca9aa0707338b2010ae7f1dfdcb9016f3d0
Samin Ahmed <s.ahmedd99@gmail.com>
2025-08-26 10:44:05 +0100

    implement CORS policy and update API call for member retrieval

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,17 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,20 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = 'Dating app';
+  protected members = signal<any>([]);
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers() {
+    try {
+    return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 }
