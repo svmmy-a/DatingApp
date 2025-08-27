9981d7f1b6f21fd97007c17c8fb5445450dfb683
Samin Ahmed <s.ahmedd99@gmail.com>
2025-08-27 11:23:38 +0100

    add Nav component with template and styles; integrate into app component

---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css'
+})
+export class Nav {
+  protected creds: any = {}
+
+    login() {
+      console.log(this.creds);
+    }
+  }
